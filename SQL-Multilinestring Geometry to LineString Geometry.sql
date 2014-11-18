d32e2465c8ccdc240d431d41fe4e1f363189878e
Prakhar Gupta <mywolfdale@gmail.com>
2014-11-18 18:54:43 +0530

    Update SQL-Multilinestring Geometry to LineString Geometry.sql

--- SQL-Multilinestring Geometry to LineString Geometry.sql
+++ SQL-Multilinestring Geometry to LineString Geometry.sql
@@ -1,3 +1,6 @@
+##My Shapefile is in Multilinestring and shape file is not noded, therefore i need to convert it to linestring Geom to work 
+## Refer to PgRouting Documentation (How to handle unnoded shapefiles)
+
 INSERT INTO network.ind_roads_linestring(id, geom)
 SELECT id, (ST_Dump(geom)) .geom
 FROM network.ind_road;
