ae3ba32aaf0ed0214474a78c0dcae38499ff7a7f
Prakhar Gupta <mywolfdale@gmail.com>
2014-11-18 18:55:33 +0530

    Update SQL-Multilinestring Geometry to LineString Geometry.sql

--- SQL-Multilinestring Geometry to LineString Geometry.sql
+++ SQL-Multilinestring Geometry to LineString Geometry.sql
@@ -1,6 +1,6 @@
 ##My Shapefile is in Multilinestring and shape file is not noded, therefore i need to convert it to linestring Geom to work 
-## Refer to PgRouting Documentation (How to handle unnoded shapefiles)
-
+## Refer to PgRouting Documentation (for handling unnoded shapefiles)
+##This SQL Query is uses PostGIS function
 INSERT INTO network.ind_roads_linestring(id, geom)
 SELECT id, (ST_Dump(geom)) .geom
 FROM network.ind_road;
