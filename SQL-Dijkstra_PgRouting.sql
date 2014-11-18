eeb0799d9701e303528fa39dee46192585c18ae4
Prakhar Gupta <mywolfdale@gmail.com>
2014-11-18 18:51:10 +0530

    Update and rename SQL-Dijkstra.sql to SQL-Dijkstra_PgRouting.sql

--- SQL-Dijkstra_PgRouting.sql
+++ SQL-Dijkstra_PgRouting.sql
@@ -1,3 +1,7 @@
+## Query 
+
+
+
 SELECT seq, id1 AS node, id2 AS edge, cost,geom
   FROM pgr_dijkstra(
     'SELECT   id::int4, source::int4  , target::int4 , st_length(geom) AS cost FROM network.ind_roads_linestring_noded',
